72b5d21e1a8338128ea60c9f237097f41667e424
Léo Masson <lmasson@edhebi.info>
2022-01-03 04:22:14 +0100

    day 2 in julia

--- 2021/julia/day01.jl
+++ 2021/julia/day01.jl
@@ -1,18 +1,14 @@
 include("common.jl")
 
-function part1()
-  getinput(1) do f
-    data = parse.(Int, readlines(f))
-    count(>(0), diff(data))
-  end
+part1() = getinput(1) do f
+  data = parse.(Int, readlines(f))
+  count(>(0), diff(data))
 end
 
-function part2()
-  getinput(1) do f
-    data = parse.(Int, readlines(f))
-    window = data[1:end-2] + data[2:end-1] + data[3:end]
-    count(>(0), diff(window))
-  end
+part2() = getinput(1) do f
+  data = parse.(Int, readlines(f))
+  window = data[1:end-2] + data[2:end-1] + data[3:end]
+  count(>(0), diff(window))
 end
 
 println("Part 1: ", part1())
