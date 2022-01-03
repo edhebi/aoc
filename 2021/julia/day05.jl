8cc42035c9978a03f3afbe8315b818967859d5cc
Léo Masson <lmasson@edhebi.info>
2022-01-03 18:43:29 +0100

    day 6 in julia

--- 2021/julia/day05.jl
+++ 2021/julia/day05.jl
@@ -1,29 +1,28 @@
 include("common.jl")
 
 function parseline(line)
-    x1, y1, x2, y2 = parse.(Int, split(line, r",|->"))
-    (; x1, y1, x2, y2)
+  x1, y1, x2, y2 = parse.(Int, split(line, r",|->"))
+  (; x1, y1, x2, y2)
 end
 
 indices(a, b) = a == b ? a : a:sign(b-a):b
 getpoints(l) = (Tuple ∘ vcat).(indices(l.x1, l.x2), indices(l.y1, l.y2))
 
 function intersections(lines)
-    counted = reduce(mergewith(+), Dict(z=>1 for z=getpoints(line)) for line=lines)
-    count(>(1), values(counted))
+  counted = reduce(mergewith(+), Dict(z=>1 for z=getpoints(line)) for line=lines)
+  count(>(1), values(counted))
 end
 
 part1() = getinput(5) do f
-    lines = parseline.(readlines(f))
-    aligned = filter(l -> l.x1 == l.x2 || l.y1 == l.y2, lines)
-    intersections(aligned)
+  lines = parseline.(readlines(f))
+  aligned = filter(l -> l.x1 == l.x2 || l.y1 == l.y2, lines)
+  intersections(aligned)
 end
 
 part2() = getinput(5) do f
-    lines = parseline.(readlines(f))
-    intersections(lines)
+  lines = parseline.(readlines(f))
+  intersections(lines)
 end
 
 println("Part 1: ", part1())
 println("Part 2: ", part2())
-
---
+++ 2021/julia/day06.jl
@@ -0,0 +1,16 @@
+include("common.jl")
+
+function simulate(fishes, N)
+  pop = mergewith(+, Dict.(fishes .=> 1)...)
+  next((k, n)) = k > 0 ? Dict(k-1 => n) : Dict(6 => n, 8 => n)
+  for _ = 1:N
+    pop = mergewith(+, next.(collect(pop))...)
+  end
+  sum(values(pop))
+end
+
+part1() = simulate(parse.(Int, split(getinput(6), ',')), 80)
+part2() = simulate(parse.(Int, split(getinput(6), ',')), 256)
+
+println("Part 1: ", part1())
+println("Part 2: ", part2())
